11a8cd74aaf90cf3f3cdc996b8c703eafb97c14b
simo <simone.riolfo07@gmail.com>
2026-04-27 21:47:44 +0200

    Refactor main function for improved error handling and clarity

--- src/main.rs
+++ src/main.rs
@@ -1,12 +1,13 @@
 use std::env;
 use std::fs;
 use std::process;
+use std::error::Error;
 
 
 fn main() {
     let args: Vec<String> = env::args().collect();
     
-    let config= Config::new(&args).unwrap_or_else(|err| {
+    let config: Config= Config::new(&args).unwrap_or_else(|err| {
         println!("Problem parsing arguments: {}", err);
         process::exit(1);
     });
@@ -14,10 +15,19 @@ fn main() {
     println!("Query: {:?}", config.query);
     println!("Filename {:?}", config.filename);
 
-    let contents: String = fs::read_to_string(config.filename)
-        .expect("Something gone wrong while reading the file.");
+    if let Err(e) = run(config) {
+        println!("Application error {:?}", e);
+        process::exit(1);
+    }
+}
+
+
+fn run(config: Config) -> Result<(), Box<dyn Error>> {
+    let contents: String = fs::read_to_string(config.filename)?;
 
     println!("Contents {:?}", contents);
+
+    Ok(())
 }
 
 
