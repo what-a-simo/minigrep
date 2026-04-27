e388154ce508087f72ba539d12c1d4470b298eb8
simo <simone.riolfo07@gmail.com>
2026-04-27 21:59:47 +0200

    Refactor main function and add Config struct for improved clarity and error handling

---
+++ src/lib.rs
@@ -0,0 +1,50 @@
+use std::fs;
+use std::error::Error;
+
+
+pub fn run(config: Config) -> Result<(), Box<dyn Error>> {
+    let contents: String = fs::read_to_string(config.filename)?;
+
+    println!("Contents {:?}", contents);
+
+    Ok(())
+}
+
+
+pub struct Config {
+    pub query: String,
+    pub filename: String,
+}
+
+
+impl Config {
+    pub fn new(args: &[String]) -> Result<Config, &str> {
+        if args.len() > 3 {
+            return  Err("Not enough arguments");
+        }
+
+        let query: String = args[1].clone();
+        let filename: String = args[2].clone();
+
+        Ok(Config {query, filename})
+    }
+}
+
+
+pub fn search<'a>(query: &str, contents: &'a str) -> Vec(&str) {
+    vec![]
+}
+
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn one_result() {
+        let query = "duct";
+        let contents = "\none";
+
+        assert_eq!(vec!["safe, fast, productive."], search(query, contents));
+    }
+}
--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,7 @@
 use std::env;
-use std::fs;
 use std::process;
-use std::error::Error;
+
+use minigrep::Config;
 
 
 fn main() {
@@ -15,37 +15,8 @@ fn main() {
     println!("Query: {:?}", config.query);
     println!("Filename {:?}", config.filename);
 
-    if let Err(e) = run(config) {
+    if let Err(e) = minigrep::run(config) {
         println!("Application error {:?}", e);
         process::exit(1);
-    }
-}
-
-
-fn run(config: Config) -> Result<(), Box<dyn Error>> {
-    let contents: String = fs::read_to_string(config.filename)?;
-
-    println!("Contents {:?}", contents);
-
-    Ok(())
-}
-
-
-struct Config {
-    query: String,
-    filename: String,
-}
-
-
-impl Config {
-    fn new(args: &[String]) -> Result<Config, &str> {
-        if args.len() > 3 {
-            return  Err("Not enough arguments");
-        }
-
-        let query: String = args[1].clone();
-        let filename: String = args[2].clone();
-
-        Ok(Config {query, filename})
     }
 }
